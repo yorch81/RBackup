5c7852d933ccb26e7c4dc859605ccf363168210b
YoRcH <the.yorch@gmail.com>
2015-01-15 15:22:30 -0600

    License

--- src/main/java/net/yorch/rbackup/App.java
+++ src/main/java/net/yorch/rbackup/App.java
@@ -6,12 +6,35 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
+ * App
+ * 
  * Application Class
+ * 
+ * Copyright 2015 Jorge Alberto Ponce Turrubiates
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @category   App
+ * @package    net.yorch.rbackup
+ * @copyright  Copyright 2015 JAPT
+ * @license    http://www.apache.org/licenses/LICENSE-2.0
+ * @version    1.0.0, 2015-12-01
+ * @author     <a href="mailto:the.yorch@gmail.com">Jorge Alberto Ponce Turrubiates</a>
  */
 public class App {
 	public static void main( String[] args ){
 		Properties config = new Properties();
+		
 		try {
 			if (args.length == 1){
 				config.load(new FileInputStream(args[0]));
